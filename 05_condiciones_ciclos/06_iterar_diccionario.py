781f8145dd97ba8aaaab43ed14828f14ea2b76f8
Francisco Mendoza <frgmendoza@gmail.com>
2022-04-18 07:23:13 -0600

    basic concepts and SOLID

--- 05_condiciones_ciclos/06_iterar_diccionario.py
+++ 05_condiciones_ciclos/06_iterar_diccionario.py
@@ -3,13 +3,13 @@ lenguaje = {
     "creador": "Guido Van Rossum"
 }
 
-
+""""
 # Iterar un diccionario
 for llave in lenguaje:
     print(llave)
     print(lenguaje[llave])
-
-
+"""
+"""
 # Iterar un diccionario usando keys()
 for valor in lenguaje.keys():
     print(valor)
@@ -19,7 +19,7 @@ for valor in lenguaje.keys():
 for valor in lenguaje.values():
     print(valor)
 
-
+"""
 # Iterar un diccionario usando items()
-for llave, valor in lenguaje.items():
-    print(llave, valor)
+for elemento in lenguaje.items():
+    print(elemento)
